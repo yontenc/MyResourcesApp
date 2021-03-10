e4f57edbee8d5cf2ee04d32bc89d59e3e8f8af18
yontenc <yontenchoden1991@gmail.com>
2021-03-10 14:03:58 +0600

    Added Product COntroller

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyResourcesApp.Models;
+
+namespace MyResourcesApp.Controllers
+{
+    public class ProductController : Controller
+
+    {
+        private readonly ApplicationContext _db;
+
+        public ProductController(ApplicationContext db)
+        {
+            _db = db;
+        }
+        //site inforamtion
+        public IActionResult RegisterProduct()
+        {
+            var productInfoList = _db.product.ToList();
+            return View(productInfoList);
+        }
+    }
+}
+
