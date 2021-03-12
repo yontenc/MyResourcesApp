9eae823a8f1b29ad6caede19bec7d2b97a48295a
yontenc <yontenchoden1991@gmail.com>
2021-03-12 12:44:32 +0600

    Models folder for product model

---
+++ Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyResourcesApp.Models
+{
+    public class Product
+    {
+        [Key]
+        [Required(ErrorMessage = "Product name is required")]
+        public String productName { get; set; }
+
+        [Required(ErrorMessage = "Price per unit is required")]
+        public decimal PricePerUnit { set; get; }
+
+        [Required(ErrorMessage = "Transport rate is required")]
+        public decimal TransportRate { set; get; }
+    }
+}
+
