50ccd490f88527f44c498bcbc8660f1f145456d3
yontenc <yontenchoden1991@gmail.com>
2021-03-16 17:29:48 +0600

    changes in deposit history

---
+++ Models/DepositAdance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyResourcesApp.Models
+{
+    public class DepositAdance
+    {
+        [Key]
+        [Required(ErrorMessage = "Customer ID is required")]
+        public String CustomerCID { get; set; }
+
+        [Required(ErrorMessage = "Amount is required")]
+        public decimal Amount { set; get; }
+
+        public decimal Balance { set; get; }
+    }
+}
