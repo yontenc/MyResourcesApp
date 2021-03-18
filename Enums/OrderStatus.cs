0aeaf3f7a429358bbdd5ac33e1dbbbc6bc56c39c
yontenc <yontenchoden1991@gmail.com>
2021-03-18 14:56:28 +0600

    After Place order screens added

---
+++ Enums/OrderStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    public enum OrderStatus 
+    {
+        Pending ='P',
+        Delivered='D'
+    }
+    //char status = (char)Enums.DivisionStatus.Active;
+}
